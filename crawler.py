bce56728bfd7dffbb938f71bd89a185621c0dfe8
Manjit Kumar <manav9316@users.noreply.github.com>
2013-12-15 02:12:25 +0530

    Update crawler.py
    improved to work with relative links. :)

--- crawler.py
+++ crawler.py
@@ -1,19 +1,26 @@
 # Web Crawler
 
 import urllib2
+from bs4 import BeautifulSoup
+
 ###
 # urllib is a python module consiting
 # of important url related functions
 # which are helpful in Web Crawler's working.
 ###
 
-from bs4 import BeautifulSoup
 
-def get_all_links(page):
+
+
+def get_all_links(page,seed):
     links = []
     for link in page.find_all('a'):
-        links.append(link.get('href'))
+        url = (link.get('href'))
+        if url.find("http") == -1:
+            url = seed+"/"+url
+        links.append(str(url))
     return links
+
 
 def extract_page(seed):
     soup = BeautifulSoup(urllib2.urlopen(seed).read())
@@ -23,7 +30,6 @@ def extract_page(seed):
 def union(l1,l2):
     for i in l2:
         if i not in l1:
-            print i
             l1.append(i)      
     return l1
     
@@ -33,11 +39,10 @@ def crawling(seed):
     try:
         while tocrawl:
             url = tocrawl.pop(0)
-            if url.find("http") == -1:
-                url = seed+"/"+url
             if url not in crawled:
-                tocrawl = union(tocrawl,get_all_links(extract_page(url)))
+                tocrawl = union(tocrawl,get_all_links(extract_page(url),seed))
                 crawled.append(url)
+                print url
     except:
         pass
     return  crawled
